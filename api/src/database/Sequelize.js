3f53b73c40be96ace7230f9d442097f4b31c63ee
gandhyman <gandhycoder@gmail.com>
2022-12-09 05:26:16 -0700

    Update: Postgres env vars

--- api/src/database/Sequelize.js
+++ api/src/database/Sequelize.js
@@ -1,36 +1,36 @@
 // Se conecta a la BD con las credenciales de el archivo .env
 
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGPORT } = process.env;
 
-let sequelize =
-  process.env.NODE_ENV === "production"
+const sequelize =
+  process.env.NODE_ENV === 'production'
     ? new Sequelize({
-      database: DB_NAME,
-      dialect: "postgres",
-      host: DB_HOST,
-      port: 5432,
-      username: DB_USER,
-      password: DB_PASSWORD,
-      pool: {
-        max: 3,
-        min: 1,
-        idle: 10000,
-      },
-      dialectOptions: {
-        ssl: {
-          require: true,
-          // Ref.: https://github.com/brianc/node-postgres/issues/2009
-          rejectUnauthorized: false,
+        database: PGDATABASE,
+        dialect: 'postgres',
+        host: PGHOST,
+        port: PGPORT,
+        username: PGUSER,
+        password: PGPASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
         },
-        keepAlive: true,
-      },
-      ssl: true,
-    })
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
     : new Sequelize(
-      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-      { logging: false, native: false }
-    );
+        `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`,
+        { logging: false, native: false }
+      );
 
 module.exports = sequelize;
